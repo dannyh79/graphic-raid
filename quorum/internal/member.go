a353cd044ded831f481af8e58d5273f5e4f4acc5
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-04-28 01:13:34 +0800

    Add board.NewController

--- quorum/internal/member.go
+++ quorum/internal/member.go
@@ -7,15 +7,19 @@ import (
 )
 
 type BoardMember struct {
-	Id         string
-	Writer     io.Writer
-	WantToLead func() bool
+	Id           string
+	Writer       io.Writer
+	WantToLead   func() bool
+	Ack          chan string
+	ReadyToElect chan bool
 }
 
 type MemberParams struct {
-	Id         string
-	Writer     io.Writer
-	WantToLead func() bool
+	Id           string
+	Writer       io.Writer
+	WantToLead   func() bool
+	Ack          chan string
+	ReadyToElect chan bool
 }
 
 func NewMember(p MemberParams) {
@@ -23,14 +27,58 @@ func NewMember(p MemberParams) {
 	pl := func(s string) { fmt.Fprintln(p.Writer, s) }
 
 	m := BoardMember{
-		WantToLead: p.WantToLead,
+		WantToLead:   p.WantToLead,
+		Ack:          p.Ack,
+		ReadyToElect: p.ReadyToElect,
 	}
 
 	pl(prefix + "Hi")
+	m.Ack <- p.Id
 
-	if m.WantToLead() {
-		pl(prefix + "I want to be leader")
+	select {
+	case <-m.ReadyToElect:
+		if m.WantToLead() {
+			pl(prefix + "I want to be leader")
+		}
 	}
 }
 
 func FiftyFifty() bool { return rand.Intn(2) == 0 }
+
+type Controller struct {
+	members      int
+	Ack          chan string
+	ReadyToElect chan bool
+}
+
+type ControllerParams struct {
+	Members      int
+	Ack          chan string
+	ReadyToElect chan bool
+}
+
+func (c *Controller) allReadyToElect() {
+	c.ReadyToElect <- true
+}
+
+func (c *Controller) start() {
+	i := 0
+	for range c.Ack {
+		i++
+		if i == c.members {
+			c.allReadyToElect()
+		}
+	}
+}
+
+func NewController(p ControllerParams) *Controller {
+	c := Controller{
+		members:      p.Members,
+		ReadyToElect: p.ReadyToElect,
+		Ack:          p.Ack,
+	}
+
+	go c.start()
+
+	return &c
+}
--- quorum/internal/member_test.go
+++ quorum/internal/member_test.go
@@ -2,6 +2,7 @@ package board_test
 
 import (
 	"bytes"
+	"strconv"
 
 	board "github.com/dannyh79/graphic-raid/quorum/internal"
 
@@ -11,18 +12,24 @@ import (
 
 var _ = Describe("NewMember", func() {
 	var (
-		buf bytes.Buffer
-		o   func() string = func() string { return buf.String() }
-		p   board.MemberParams
+		buf          bytes.Buffer
+		o            func() string = func() string { return buf.String() }
+		p            board.MemberParams
+		ack          chan string
+		readyToElect chan bool
 	)
 
 	BeforeEach(func() {
 		buf.Reset()
 
+		ack = make(chan string)
+		readyToElect = make(chan bool)
 		p = board.MemberParams{
-			Id:         "0",
-			Writer:     &buf,
-			WantToLead: func() bool { return true },
+			Id:           "0",
+			Writer:       &buf,
+			WantToLead:   func() bool { return true },
+			Ack:          ack,
+			ReadyToElect: readyToElect,
 		}
 	})
 
@@ -35,6 +42,9 @@ var _ = Describe("NewMember", func() {
 	It(`Writes "Member 0: I want to be leader" to buffer`, func() {
 		go board.NewMember(p)
 
+		<-ack
+		readyToElect <- true
+
 		Eventually(o).Should(ContainSubstring("Member 0: I want to be leader\n"))
 	})
 
@@ -43,15 +53,47 @@ var _ = Describe("NewMember", func() {
 
 		go board.NewMember(p)
 
+		<-ack
+		readyToElect <- true
+
 		Eventually(o).Should(Not(ContainSubstring("Member 0: I want to be leader\n")))
 	})
 
 	It(`Writes to buffer in a sequential manner`, func() {
 		go board.NewMember(p)
 
+		<-ack
+		readyToElect <- true
+
 		Eventually(o).Should(ContainSubstring(
 			"Member 0: Hi\n" +
 				"Member 0: I want to be leader\n",
 		))
+	})
+})
+
+var _ = Describe("NewController", func() {
+	var (
+		n int
+		c *board.Controller
+	)
+
+	It(`Sends to c.ReadyToElect`, func() {
+		n = 2
+		p := board.ControllerParams{
+			Members:      n,
+			Ack:          make(chan string),
+			ReadyToElect: make(chan bool),
+		}
+
+		c = board.NewController(p)
+
+		go func() {
+			for i := 0; i < n; i++ {
+				c.Ack <- strconv.Itoa(i)
+			}
+		}()
+
+		Eventually(<-c.ReadyToElect).Should(BeTrue())
 	})
 })
